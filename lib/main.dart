73e85eb3caad23923009b7dfcc8e7b63ba34aee0
Yasin <mad_rock@hotmail.com>
2019-08-05 01:02:27 +0300

    liste elemanları silinebiliyor.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
@@ -40,12 +41,10 @@ class _PlatformChannelState extends State<PlatformChannel> {
   static const MethodChannel methodChannel =
       MethodChannel('com.arayan.incoming_call/calls');
 
-  //List<String> _calls;
   var _calls;
   List data;
-  //final String url = "https://ankaranakliyat.web.tr/rehber/api/customer/create.php";
-  final String url = "http://10.0.2.2/api/customer/create.php";
-  //final String getUrl="http://10.0.2.2/api/customer/read_single_number.php?number=";
+  final String url = "https://ankaranakliyat.web.tr/rehber/api/customer/create.php";
+  //final String url = "http://10.0.2.2/api/customer/create.php";
 
   @override
   initState() {
@@ -55,51 +54,42 @@ class _PlatformChannelState extends State<PlatformChannel> {
   }
 
   /// http request
-  Future<String> getSWData(String postBody) async {
-    var res = await http
-        .post(url, body: postBody, headers: {"Accept": "application/json"});
+  Future<void> getSWData(postBody) async {
+    if (postBody.length > 0) {
+      postBody = jsonEncode(postBody); //postBody.toString();
+      print(postBody);
+      var res = await http
+          .post(url, body: postBody, headers: {"Accept": "application/json"});
 
-    setState(() {
-      var resBody = json.decode(res.body);
-      data = resBody;
-    });
+      setState(() {
+        var resBody = json.decode(res.body);
+        data = resBody;
+        _showDialog("Kaydedildi",
+            data.length.toString() + " adet numara veritabanına kaydedildi");
+      });
+    }else{
+      _showDialog("Kayıt Yok", "Kaydedilecek numara yok");
+    }
 
-    return "Success!";
+    //return "Success!";
   }
 
   /// /http
 
   Future<void> _getCalls() async {
     String calls;
-    bool success2 = false;
     try {
       final String result = await methodChannel.invokeMethod('getCalls');
       calls = result;
-      print(calls);
-      success2 = true;
+      print("calls: " + calls);
     } catch (e) {
-      //on PlatformException {
       calls = 'Failed to get calls. ';
       _showDialog("Hata",
           "Aramaları alırken hata oluştu. Arama kaydına erişim yetkilerini kontrol edin");
     }
-    // if (success2) {
-    //   try {
-    //     await addContactIfNotExist(calls);
-    //     print("rehbere kaydedildi");
-    //     _showDialog("Başarılı", "Numaralar rehbere kaydedildi");
-    //   } catch (e) {
-    //     print("rehbere kaydedilemedi hata: \n" + e.toString());
-    //     _showDialog("Hata", "Rehbere kaydederken hata oluştu");
-    //   }
-    // }
     setState(() {
-      // _calls = calls.split(",").map((tel)=>(
-      //   "{\"name\":\"Musteri"+DateTime.now().toString()+"\",\"number\":" + tel.toString() + ", \"category_id\":1}"
-      //   )
-      //   );
-      // print(_calls);
       _calls = jsonDecode(calls);
+      print("_calls " + jsonEncode(_calls));
     });
   }
 
@@ -133,15 +123,15 @@ class _PlatformChannelState extends State<PlatformChannel> {
 
       customers = "[" + customers.substring(0, customers.length - 1) + "]";
 
-      try {
-        await getSWData(customers);
-        print("veritabanına kaydedildi");
-        _showDialog("Bilgi", "Veritabanına kaydedildi");
-        // print(customers);
-      } catch (e) {
-        print("veritabanına kaydedilemedi hata: \n" + e.toString());
-        _showDialog("Hata", "Veritabanına kaydedilemedi");
-      }
+      // try {
+      //   await getSWData(customers);
+      //   print("veritabanına kaydedildi");
+      //   _showDialog("Bilgi", "Veritabanına kaydedildi");
+      //   // print(customers);
+      // } catch (e) {
+      //   print("veritabanına kaydedilemedi hata: \n" + e.toString());
+      //   _showDialog("Hata", "Veritabanına kaydedilemedi");
+      // }
     } else {
       // popup yeni numara bulunamadı
       _showDialog("Bilgi", "Yeni numara bulunamadı");
@@ -171,6 +161,11 @@ class _PlatformChannelState extends State<PlatformChannel> {
     );
   }
 
+  void saveToCloud() {
+    final record = Record.fromMap(_calls);
+    record.reference.setData(_calls);
+  }
+
   @override
   Widget build(BuildContext context) {
     //bool rehbereKaydet = true, vtKaydet = true;
@@ -189,6 +184,7 @@ class _PlatformChannelState extends State<PlatformChannel> {
               final item = _calls[index];
               return Dismissible(
                   key: Key(item["name"]),
+                  direction: DismissDirection.endToStart,
                   onDismissed: (direction) {
                     setState(() {
                       _calls.removeAt(index);
@@ -204,10 +200,10 @@ class _PlatformChannelState extends State<PlatformChannel> {
                       child: Padding(
                         child: Text(
                           "Sil",
-                          style: TextStyle(color: Colors.white, fontSize: 24),
+                          style: TextStyle(color: Colors.white, fontSize: 20),
                           textAlign: TextAlign.right,
                         ),
-                        padding: EdgeInsets.all(20.0),
+                        padding: EdgeInsets.all(22.0),
                       )),
                   child: ListTile(
                     title: Text(item["name"]),
@@ -218,10 +214,33 @@ class _PlatformChannelState extends State<PlatformChannel> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _getCalls, //(){addContactIfNotExist(_calls);},
+        onPressed: () {
+          getSWData(_calls);
+        }, //()=>print("düğmeye basıldı"),//saveToCloud, //(){addContactIfNotExist(_calls);},
         tooltip: 'Kaydet',
         child: Icon(Icons.save),
       ),
     );
   }
+}
+
+class Record {
+  final String name;
+  final String number;
+  final int categoryId;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['number'] != null),
+        assert(map['category_id'] != null),
+        name = map['name'],
+        number = map['number'],
+        categoryId = map['category_id'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$name:$number:$categoryId>";
 }
